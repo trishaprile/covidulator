9954a8ed2af97e31d822d94e0e5a4fa10d2f1658
trishaprile <trishaprilee@gmail.com>
2021-02-17 14:35:24 -0800

    update home page

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const questions = [
+  {
+    question: "",
+    number: 1,
+    answers: [
+        { answer: "", points: 0},
+        { answer: "", points: 0},
+    ]
+  },
+  {
+    question: "",
+    number: 2,
+    answers: [
+        { answer: "", points: 0},
+        { answer: "", points: 0},
+    ]
+  },
+];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import './Home.css';
+import './Home.scss';
+import CovidParticle from '../assets/coronaangry.png';
+
 import { Button } from 'react-bootstrap';
 
 function Home() {
+
 	return (
     <div className="home">
-      <h1>this is the home page</h1>
-      <Button href="/quiz" className="button">click me</Button>
+      <div className="background-container">
+	      <div class="particle"><img src={CovidParticle} alt="Covid Particle" /></div>
+        <div class="particle"><img src={CovidParticle} alt="Covid Particle" /></div>
+        <div class="particle"><img src={CovidParticle} alt="Covid Particle" /></div>
+        <div class="particle"><img src={CovidParticle} alt="Covid Particle" /></div>
+        <div class="particle"><img src={CovidParticle} alt="Covid Particle" /></div>
+        <div class="particle"><img src={CovidParticle} alt="Covid Particle" /></div>
+        <div class="particle"><img src={CovidParticle} alt="Covid Particle" /></div>
+        <div class="particle"><img src={CovidParticle} alt="Covid Particle" /></div>
+        <div class="particle"><img src={CovidParticle} alt="Covid Particle" /></div>
+      </div>
+      <div className="text-container">
+        <h2 className="pretitle">WHAT IS YOUR</h2>
+        <h1 className="title">COVID-19 Footprint?</h1>
+        <h3 className="cases">There have been ___ cases worldwide</h3>
+        <h3 className="subtitle">Find out if you're doing your part.</h3>
+        <Button href="/quiz" className="take-test-btn">Take the Test</Button>
+      </div>
     </div> 
   )
 }
