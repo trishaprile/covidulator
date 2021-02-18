c8f6dfccf6fea2a9239a2c3356575ce501367364
nmpham2 <nmpham2@uci.edu>
2021-02-17 19:13:55 -0800

    finished adding basic results layout

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Results.css';
+import { Button } from 'react-bootstrap';
 
 function Results() {
 	return (
@@ -24,6 +25,19 @@ function Results() {
         <h4 className="subtitle">SUGGESTIONS</h4>
         <h6 className="description">(how to improve each category)</h6>
       </div>
+
+      <div className="resources">
+        <h4 className="subtitle">RESOURCES</h4>
+        <div className="links">
+          <a href="https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/prevention.html">CDC: Suggestions to Prvent Getting Sick</a><br/>
+          <a href="https://www.cdc.gov/coronavirus/2019-ncov/your-health/effective-masks.html">CDC: Improve How Your Mask Protects You</a><br/>
+          <a href="https://www.cdc.gov/coronavirus/2019-ncov/vaccines/index.html">CDC: Information about the Covid-19 Vaccine</a><br/>
+          <a href="https://www.cdc.gov/coronavirus/2019-ncov/travelers/testing-international-air-travelers.html">CDC: Testing and International Air Travel</a><br/>
+        </div>
+      </div>
+
+      <Button href="/quiz" className="take-test-btn">Take the Test Again</Button>
+
     </div>
   )
 }
